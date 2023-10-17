9de86f8e351ab15b47eec5d4f7a346436552726b
Flagg217 <flagg217@gmail.com>
2023-10-16 18:53:09 -0700

    Added to queries and created an auth.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 
-import { QUERY_CARDS, QUERY_ME_BASIC } from '../utils/queries';
+import { QUERY_CARDS, QUERY_ME } from '../utils/queries';
 
 const Home = () => {
     const { data } = useQuery(QUERY_CARDS);
     const cards = data?.cards || [];
     console.log(cards);
-    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const { data: userData } = useQuery(QUERY_ME);
     const user = userData?.me || {};
     console.log(user);
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,46 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+    query user($username: String!) {
+        user(username: $username) {
+            _id
+            username
+            email
+            cardCount
+            cards {
+                _id
+                cardText
+                cardAuthor
+                cardCreated
+            }
+        }
+    }
+`;
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            cardCount
+            cards {
+                _id
+                cardText
+                cardAuthor
+                cardCreated
+            }
+        }
+    }
+`;
+
+export const QUERY_CARDS = gql`
+    query getCards {
+       card {
+           _id
+           cardText
+           cardAuthor
+           cardCreated
+       } 
+    }
+`;
