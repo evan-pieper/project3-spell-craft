4965a9e07129495aeb4ec9cf46167c60febd6e00
Flagg217 <flagg217@gmail.com>
2023-10-16 18:28:28 -0700

    added to Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,22 @@
+import React from "react";
+import { useQuery } from '@apollo/client';
 
+import { QUERY_CARDS, QUERY_ME_BASIC } from '../utils/queries';
+
+const Home = () => {
+    const { data } = useQuery(QUERY_CARDS);
+    const cards = data?.cards || [];
+    console.log(cards);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    const user = userData?.me || {};
+    console.log(user);
+
+    return (
+        <main>
+
+        </main>
+    );
+};
 
 
 
