2fd08ef88c087129141b4117c053ffa2e99ef815
evan-pieper <evanjpieper@gmail.com>
2023-10-19 19:51:45 -0700

    ADD_USER query is causing the errors. Currently tinkering

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+//import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  //const [addUser, { error, data }] = useMutation(ADD_USER);
   return(
     <div>
       <h1>Here is the Signup Page!</h1>
